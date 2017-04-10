4d74990eadfac28ef0450c70878c829bfaae928d
zuzannna <zklyszejko@gmail.com>
2017-04-10 13:37:35 -0400

    added find_labels function

--- file_utilities.py
+++ file_utilities.py
@@ -10,3 +10,13 @@ def write_json(data, filepath):
     with open(filepath, "w") as f:
         json.dump(data, f)
 
+def find_labels(word):
+    for i,flavor in enumerate(flavor_table):
+        if word in flavor_table[i].values():
+            if flavor_table[i]['level_3'] == word:
+                return flavor_table[i].values()
+            elif flavor_table[i]['level_2'] == word:
+                return [flavor_table[i]['level_1'], flavor_table[i]['level_2']]
+            else:
+                return [flavor_table[i]['level_1']]
+
