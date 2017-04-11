e8e770d5e058f5c5fb5417ed58d9114593e3dbc2
zuzannna <zklyszejko@gmail.com>
2017-04-11 16:32:57 -0400

    updated find_labels function to account for processed and unprocessed labels

--- file_utilities.py
+++ file_utilities.py
@@ -10,12 +10,15 @@ def write_json(data, filepath):
     with open(filepath, "w") as f:
         json.dump(data, f)
 
-def find_labels(word):
+def find_labels(word, flavor_table, flavor_table_processed):
     for i,flavor in enumerate(flavor_table):
-        if word in flavor_table[i].values():
-            if flavor_table[i]['level_3'] == word:
+        if word in flavor_table_processed[i].values():
+            if flavor_table_processed[i]['level_3'] and \
+            word in flavor_table_processed[i]['level_3']: 
+                #'None' returns a false in a conditional statement 
+                # so we need to use this
                 return flavor_table[i].values()
-            elif flavor_table[i]['level_2'] == word:
+            elif word in flavor_table_processed[i]['level_2']:
                 return [flavor_table[i]['level_1'], flavor_table[i]['level_2']]
             else:
                 return [flavor_table[i]['level_1']]
