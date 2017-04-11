23130c7e36c9cddd7c74e5ab7a33f4dc6322fe93
zuzannna <zklyszejko@gmail.com>
2017-04-11 17:13:05 -0400

    added function descriptions

--- file_utilities.py
+++ file_utilities.py
@@ -1,16 +1,50 @@
 import json
 
+"""
+These are some helper functions for Coffee Flavor project.
+"""
 
 def open_json(filepath):
+	"""
+	Returns open .json file in python as a list.
+
+	:param: .json file path
+
+	:returns: list
+
+	:rvalue: str
+	"""
     with open(filepath) as f:
         notes = json.load(f)
     return notes
 
+
 def write_json(data, filepath):
+	"""
+	Writes .json file from data dump.
+
+	:param: data, .json file path
+
+	:returns: .json file
+
+	"""
     with open(filepath, "w") as f:
         json.dump(data, f)
 
+
 def find_labels(word, flavor_table, flavor_table_processed):
+	"""
+	Searches flavor_table_processed containing tokenized, lemmatized 
+	and stemmed words to find appropriate tags and returns 
+	corresponding non processed tags from flavor_table. 
+
+	:param: str, list of dictionaries, list of dictionaries
+
+	:returns: list of lists
+
+	:rvalue: str
+	"""
+
     for i,flavor in enumerate(flavor_table):
         if word in flavor_table_processed[i].values():
             if flavor_table_processed[i]['level_3'] and \
@@ -19,7 +53,8 @@ def find_labels(word, flavor_table, flavor_table_processed):
                 # so we need to use this
                 return flavor_table[i].values()
             elif word in flavor_table_processed[i]['level_2']:
-                return [flavor_table[i]['level_1'], flavor_table[i]['level_2']]
+                return [flavor_table[i]['level_1'], \
+                flavor_table[i]['level_2']]
             else:
                 return [flavor_table[i]['level_1']]
 
