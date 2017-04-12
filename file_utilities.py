2f7c1f00b755f856a2135b121c400763a6ac3334
zuzannna <zklyszejko@gmail.com>
2017-04-11 23:17:53 -0400

    improved search speed of find_label by first searching through set of all coffee flavors from flavor wheel

--- file_utilities.py
+++ file_utilities.py
@@ -5,56 +5,65 @@ These are some helper functions for Coffee Flavor project.
 """
 
 def open_json(filepath):
-	"""
-	Returns open .json file in python as a list.
+    """
+    Returns open .json file in python as a list.
 
-	:param: .json file path
+    :param: .json file path
 
-	:returns: list
+    :returns: list
 
-	:rvalue: str
-	"""
+    :rvalue: str
+    """
     with open(filepath) as f:
         notes = json.load(f)
     return notes
 
 
 def write_json(data, filepath):
-	"""
-	Writes .json file from data dump.
+    """
+    Writes .json file from data dump.
 
-	:param: data, .json file path
+    :param: data, .json file path
 
-	:returns: .json file
+    :returns: .json file
 
-	"""
+    """
     with open(filepath, "w") as f:
         json.dump(data, f)
 
 
 def find_labels(word, flavor_table, flavor_table_processed):
-	"""
-	Searches flavor_table_processed containing tokenized, lemmatized 
-	and stemmed words to find appropriate tags and returns 
-	corresponding non processed tags from flavor_table. 
+    from list_flavors import list_flavors
+    """
+    Searches flavor_table_processed containing tokenized, lemmatized 
+    and stemmed words to find appropriate tags and returns 
+    corresponding non processed tags from flavor_table. 
 
-	:param: str, list of dictionaries, list of dictionaries
+    :param: str, list of dictionaries, list of dictionaries
 
-	:returns: list of lists
+    :returns: list of lists
 
-	:rvalue: str
-	"""
+    :rvalue: str
+    """
 
-    for i,flavor in enumerate(flavor_table):
-        if word in flavor_table_processed[i].values():
-            if flavor_table_processed[i]['level_3'] and \
-            word in flavor_table_processed[i]['level_3']: 
+    # First check if word is in the flavor wheel at all
+    flavors = list_flavors(flavor_table_processed, 1)+\
+    list_flavors(flavor_table_processed, 2)+\
+    list_flavors(flavor_table_processed, 3)
+    flavors = set(flavors)
+
+    if word in flavors:
+        # If it is search through the list to find the match
+        for i,flavor in enumerate(flavor_table):
+            if word in flavor_table_processed[i].values():
+                if flavor_table_processed[i]['level_3'] and \
+                word in flavor_table_processed[i]['level_3']: 
                 #'None' returns a false in a conditional statement 
                 # so we need to use this
-                return flavor_table[i].values()
-            elif word in flavor_table_processed[i]['level_2']:
-                return [flavor_table[i]['level_1'], \
-                flavor_table[i]['level_2']]
-            else:
-                return [flavor_table[i]['level_1']]
+                    return flavor_table[i].values()
+                elif word in flavor_table_processed[i]['level_2']:
+                    return [flavor_table[i]['level_1'], \
+                    flavor_table[i]['level_2']]
+                else:
+                    return [flavor_table[i]['level_1']]
 
