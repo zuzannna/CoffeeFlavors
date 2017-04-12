1eb4d8e0412735157cf7f76124b04a6d63ab4764
zuzannna <zklyszejko@gmail.com>
2017-04-12 10:17:24 -0400

    added test for stemmer and coffee_flavors functions

--- test_utilities.py
+++ test_utilities.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 import file_utilities as u
-
+from coffee_flavors import coffee_flavors as coffee
+from text_tokenize_lemmatize as text_
 
 #################################
 # Tests for CoffeeFlavors library
@@ -9,9 +10,30 @@ import file_utilities as u
 class TestUtilities(TestCase):
 
     def test_find_labels(self):
+
         flavor_table_ = [{u'level_1': u'Roasted', 
         u'level_2': u'Pipe Tobacco', u'level_3': None}]
         expected_output = [u'Roasted']
         actual_output = u.find_labels('Roasted', flavor_table_, 
         flavor_table_)
+        self.assertEqual(expected_output, actual_output)
+
+
+    def test_coffee_flavors(self):
+
+        text = 'blackberries with hints of pineapple'
+        expected_output = [[u'Fruity', u'Berry', u'Blackberry'],
+        [u'Fruity', u'Other Fruit', u'Pineapple']]
+        actual_output = coffee.coffee_flavors(text)
+        self.assertEqual(expected_output, actual_output)
+
+
+    def test_tokenizer_lemmatizer(self):
+
+
+    def test_stemmer(self):
+        
+        tokens = [u'Fruity', u'Other Fruit', u'Pineapple']
+        expected_output = [u'Fruiti', u'Other Fruit', u'Pineappl']
+        actual_output = text_.stemmer(tokens)
         self.assertEqual(expected_output, actual_output)
