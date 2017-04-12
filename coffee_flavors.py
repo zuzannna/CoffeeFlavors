5aa24d239f2d8e7ef288d8c3c7de74fb70153b99
zuzannna <zklyszejko@gmail.com>
2017-04-12 10:00:40 -0400

    added def coffee_flavors with description

--- coffee_flavors.py
+++ coffee_flavors.py
@@ -8,18 +8,42 @@ Wrapper function that called from the terminal will ask for an input
 (a sentence / coffee review) and after comparing it to the flavor table 
 will return a list of tags.
 
+Calling the coffee_flavors(text) from another function will also return
+list of unicode strings with tags.
+
 :params: str 
 
 :returns: list of lists
 
-:rvalue: str 
-
+:rvalue: unicode 
 """
 
+def coffee_flavors(text):
+	spacy_parser = English()
+	stopwords=[',', '.', '!', ';', ':', '?','and','a','-']
+	flavor_table = open_json(filepath='flavor_table.json')
+	flavor_table_processed = \
+	open_json(filepath='flavor_table_processed.json')
 
-spacy_parser = English()
+	tags = []
+	sentence = text
+
+	tokenized_sentence = tokenizer_lemmatizer(
+	            unicode(sentence), spacy_parser, stopwords=stopwords)
+	stemmed_tokenized_sentence = stemmer(tokenized_sentence)
+
+
+	for word in stemmed_tokenized_sentence:   
+	    tags.append(find_labels(word, flavor_table, 
+	    	flavor_table_processed))
+	tags = filter(None, tags)
+
+	return tags
 
 if __name__ == '__main__':
+
+	spacy_parser = English()
+	stopwords=[',', '.', '!', ';', ':', '?','and','a','-']
 	
 	flavor_table = open_json(filepath='flavor_table.json')
 	flavor_table_processed = \
@@ -29,8 +53,7 @@ if __name__ == '__main__':
 	sentence = raw_input()
 
 	tokenized_sentence = tokenizer_lemmatizer(
-	            unicode(sentence), spacy_parser, stopwords=
-	            [',', '.', '!', ';', ':', '?','and','a','-'])
+	            unicode(sentence), spacy_parser, stopwords=stopwords)
 	stemmed_tokenized_sentence = stemmer(tokenized_sentence)
 
 
