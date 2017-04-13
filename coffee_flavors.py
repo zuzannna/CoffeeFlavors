fcddca8e0ff626df5ad076b5aaaa72ee22cee69d
zuzannna <zklyszejko@gmail.com>
2017-04-13 10:07:56 -0400

    minor spelling errors

--- coffee_flavors.py
+++ coffee_flavors.py
@@ -3,23 +3,24 @@ from file_utilities import open_json, write_json, find_labels
 from text_tokenize_lemmatize import tokenizer_lemmatizer, stemmer
 from spacy.en import English
 
-"""
-Wrapper function that called from the terminal will ask for an input 
-(a sentence / coffee review) and after comparing it to the flavor table 
-will return a list of tags.
-
-Calling the coffee_flavors(text) from another function will also return
-list of unicode strings with tags.
-
-:params: str 
-
-:returns: list of lists
-
-:rvalue: unicode 
-"""
 
 def coffee_flavors(text, flavor_table=None, flavor_table_processed=None,
-	parser=None, stopwords=None):
+					parser=None, stopwords=None):
+	
+	"""
+	Wrapper function that called from the terminal will ask for an input 
+	(a sentence / coffee review) and after comparing it to the flavor table 
+	will return a list of tags.
+
+	Calling the coffee_flavors(text) from another function will also return
+	list of unicode strings with tags.
+
+	:params: str 
+
+	:returns: list of lists
+
+	:rvalue: unicode 
+	"""
 	
 	if parser is None:
 		parser = English()
@@ -51,12 +52,12 @@ def coffee_flavors(text, flavor_table=None, flavor_table_processed=None,
 
 if __name__ == '__main__':
 
-	spacy_parser = English()
+	parser = English()
 	stopwords=[',', '.', '!', ';', ':', '?','and','a','-']
 	
-	flavor_table = open_json(filepath='flavor_table.json')
+	flavor_table = open_json(filepath='data/flavor_table.json')
 	flavor_table_processed = \
-	open_json(filepath='flavor_table_processed.json')
+	open_json(filepath='data/flavor_table_processed.json')
 
 	tags = []
 	sentence = raw_input()
